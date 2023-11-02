c0035f7bfd22260ca04faa4e7e48b69524200bc5
Diomedes Ignacio Domínguez Ureña <diomedes.dominguez@gmail.com>
2023-11-02 17:01:16 -0500

    Improved resolution and comments.

--- CamCap/Program.cs
+++ CamCap/Program.cs
@@ -9,12 +9,19 @@ class Program
 {
     static void Main()
     {
+        //Select the source and the compatible API
         using var capture = new VideoCapture(0, VideoCapture.API.DShow);
+
+        //Set the picture resolution
+        capture.Set(CapProp.FrameWidth, 3840);
+        capture.Set(CapProp.FrameHeight, 2160);
+
         while (true)
         {
-            var filename = $"captura_{DateTime.Now:yyMMddHHmmss}.jpeg";
+            var filename = $"{DateTime.Now:yyMMddHHmmss}.jpeg";//Set image file name
             var image = capture.QueryFrame(); //take a picture
-            image.Save(filename);
+            image.Save(filename); // save the picture
+            Console.WriteLine($"File saved {filename}"); // Print in console the file name just for log
             Thread.Sleep(5000*60); // wait 5 minutes
         }
     }
