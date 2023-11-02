a378f79a4f096a7fbd939ef149eeeba1af39e6ad
Diomedes Ignacio Domínguez Ureña <diomedes.dominguez@gmail.com>
2023-11-02 16:29:53 -0500

    Take 1 picture every 5 minutes.

---
+++ CamCap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+class Program
+{
+    static void Main()
+    {
+        using var capture = new VideoCapture(0, VideoCapture.API.DShow);
+        while (true)
+        {
+            var filename = $"captura_{DateTime.Now:yyMMddHHmmss}.jpeg";
+            var image = capture.QueryFrame(); //take a picture
+            image.Save(filename);
+            Thread.Sleep(5000*60); // wait 5 minutes
+        }
+    }
+}
